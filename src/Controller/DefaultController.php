2f2500d6cdf54c3982b8a7a518e5bf0a59189c5b
Yvan COLLIN <yvancollin@gmail.com>
2020-12-23 17:22:01 +0100

    partition twig

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/accueil", name="default_index",methods={"GET"})
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
