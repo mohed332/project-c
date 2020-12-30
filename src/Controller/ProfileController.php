98502aad0818dda189a8029e7f7ea5c29141a8f6
mohed332 <mohed332@gmail.com>
2020-12-30 20:44:07 +0100

    comit profil et panier

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminController extends AbstractController
+class ProfileController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/profile", name="profile")
      */
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig',[
-        'controller_name' => 'AdminController',
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'ProfileController',
         ]);
     }
 }
